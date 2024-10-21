531cba9a79c34052387acc51b95208079a8a9312
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-10-21 22:42:22 +0530

    Piece Enum Complete

--- src/main/java/lk/edu/tictacgame/tictactoe/Servers/Piece.java
+++ src/main/java/lk/edu/tictacgame/tictactoe/Servers/Piece.java
@@ -1,4 +1,15 @@
 package lk.edu.tictacgame.tictactoe.Servers;
 
 public enum Piece {
+    X, O, EMPTY;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case X: return "X";
+            case O: return "O";
+            case EMPTY: return "";
+        }
+        return "";
+    }
 }
